b0cfda8cfa70c155303bffe513ab71265a94ec42
Eesa Zahed <eesa.zahed@gmail.com>
2025-07-22 16:52:52 -0500

    added gitignore

--- target/debug/deps/rust_mp3-9caf5188fc69fd65.d
+++
@@ -1,5 +0,0 @@
-/Users/kid/Documents/dev/rust_mp3/target/debug/deps/rust_mp3-9caf5188fc69fd65.d: src/main.rs
-
-/Users/kid/Documents/dev/rust_mp3/target/debug/deps/rust_mp3-9caf5188fc69fd65: src/main.rs
-
-src/main.rs:
--- target/debug/rust_mp3.d
+++
@@ -1,1 +0,0 @@
-/Users/kid/Documents/dev/rust_mp3/target/debug/rust_mp3: /Users/kid/Documents/dev/rust_mp3/src/main.rs
