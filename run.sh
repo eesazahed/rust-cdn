e153f4972d5416c1eb86627ab8701e2254fba5dd
Eesa Zahed <eesa.zahed@gmail.com>
2025-07-22 22:49:32 -0500

    Stop tracking run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /Users/path || exit 1
-export $(grep -E '^PORT=' .env | xargs)
-PORT=${PORT:-12345}
-kill -9 $(lsof -ti ":$PORT") 2>/dev/null
-nohup cargo run > /tmp/rustmp3.log 2>&1 &
-echo "Go to http://0.0.0.0:$PORT"
